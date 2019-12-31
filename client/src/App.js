ba5b1dbc12760db8bec96afa0c867e507d48b71e
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-31 14:25:28 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div``;
+
+function App(props) {
+  return <AppWrapper></AppWrapper>;
+}
+
+export default App;
